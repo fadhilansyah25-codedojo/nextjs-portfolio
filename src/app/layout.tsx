aacc69d51d374ce5ae8c19c36e1609cd18703595
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2023-09-18 17:00:29 +0700

    add: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,20 +17,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body
+        className={`${inter.className} relative h-[5000px] bg-gray-50
+      text-gray-950
+      `}
+      >
+        {/* gradient backgroud effect */}
         <div
-          className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] 
-        w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"
+          className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] 
+        rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]"
         ></div>
         <div
-          className="bg-[#dbd7fd] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] 
-        w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
-        xl:left-[-15rem] 2xl:left[-5rem]
+          className="2xl:left[-5rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] 
+        w-[50rem] rounded-full bg-[#dbd7fd] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
+        lg:left-[-28rem] xl:left-[-15rem]
         "
         ></div>
-        <div>Navbar</div>
+
+        {/* header */}
+        <Header />
+
+        {/* Main Content */}
         {children}
-        <div>Footer</div>
+
+        {/*  */}
       </body>
     </html>
   );
